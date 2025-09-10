935d9c6f8a25016cf692461b9145011a69f4bd80
Hudson Sena <107481318+HudsonSena@users.noreply.github.com>
2025-09-10 11:11:37 -0300

    update class tarefa020 continued

--- algoritmoEstruturaDados01/tarefa020.c
+++ algoritmoEstruturaDados01/tarefa020.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
-#include <stdlib.h>
+/* Aula sobre manipulaçõ de strings
+
+#include <string.h>
 
 int main(){
-    
-}
+    char *fonte[200];
+    char *pesquisa[50];
+    printf("Texto maior e o texto menor: ");
+    scanf(" %[^\n]s", fonte);
+    scanf(" %[^\n]s", pesquisa);
+    char *resultado = strstr(fonte, pesquisa);
+    if(resultado != NULL){
+        printf("Texto menor, encontrado no texto maior.");
+    } else {
+        printf("Texto nao encontrado!");
+    }
+}*/
+
