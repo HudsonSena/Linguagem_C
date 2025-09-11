9b2f7b503ea10ca8e8484b1ea64b253d85f3407b
Hudson Sena <107481318+HudsonSena@users.noreply.github.com>
2025-09-11 17:11:39 -0300

    update class tarefa020 continued

--- algoritmoEstruturaDados01/tarefa020.c
+++ algoritmoEstruturaDados01/tarefa020.c
@@ -2,5 +2,7 @@
 #include <stdlib.h>
 
 int main(){
-    
+    void *ponteiro_duracao = malloc(sizeof(int));
+    ponteiro_duracao = 10;
+    printf("Power-up Super Salto ativado! Duracao: %d segundos", ponteiro_duracao);
 }
