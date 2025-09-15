cb266d9eb2c5511454eab5a17daa47f86b366cd1
Hudson Sena <107481318+HudsonSena@users.noreply.github.com>
2025-09-15 11:15:34 -0300

    update class tarefa020 continued

--- algoritmoEstruturaDados01/tarefa011.c
+++ algoritmoEstruturaDados01/tarefa011.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 int main(){
-    int balas[2] = {30, 50};
-    int granadas[2] = {5, 12};
-    int foguetes[2] = {2, 7};
+    int balas[2] = {30, 30};
+    int granadas[2] = {5, 5};
+    int foguetes[2] = {2, 2};
     int *inventario[3];
     inventario[0] = balas;
     inventario[1] = granadas;
     inventario[2] = foguetes;
     printf("\nInventario Inicial:");
-    printf("\nBalas: %d", *(*(inventario)));
-    printf("\nGranadas: %d", *(*(inventario + 1)));
-    printf("\nFoguetes: %d", *(*(inventario + 2)));
+    printf("\nBalas: %d/%d", *(*inventario), *((*inventario)+1));
+    printf("\nGranadas: %d/%d", *(*(inventario + 1)), *(*(inventario + 1)+1));
+    printf("\nFoguetes: %d/%d", *(*(inventario + 2)), *(*(inventario + 2)+1));
     for(int i=0; i<3; i++){
         *(*(inventario + i)) = *(*(inventario + i)) -1;
     }
     printf("\nInventario Apos o Uso:");
-    printf("\nBalas: %d", *(*(inventario)));
-    printf("\nGranadas: %d", *(*(inventario + 1)));
-    printf("\nFoguetes: %d", *(*(inventario + 2)));
+    printf("\nBalas: %d/%d", *(*inventario), *((*inventario)+1));
+    printf("\nGranadas: %d/%d", *(*(inventario + 1)), *(*(inventario + 1)+1));
+    printf("\nFoguetes: %d/%d", *(*(inventario + 2)), *(*(inventario + 2)+1));
     return 0;
 }
--- algoritmoEstruturaDados01/tarefa020.c
+++ algoritmoEstruturaDados01/tarefa020.c
@@ -3,6 +3,11 @@
 
 int main(){
     void *ponteiro_duracao = malloc(sizeof(int));
-    ponteiro_duracao = (int *) 10;
-    printf("Power-up Super Salto ativado! Duracao: %d segundos", ponteiro_duracao);
+    printf("Valor o super salto: ");
+    scanf("%d", ((int *) ponteiro_duracao));
+    printf("\nPower-up Super Salto ativado! Duracao: %d segundos", *((int *) ponteiro_duracao));
+    ponteiro_duracao = *((int *) ponteiro_duracao -3);
+    printf("\n3 segundos se passaram... Duracao restante: %d segundos", ponteiro_duracao);
+    free(ponteiro_duracao);
+    return 0;
 }
