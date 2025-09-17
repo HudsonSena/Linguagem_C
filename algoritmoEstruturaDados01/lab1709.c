7e7935693dd03533bcced0a79ea15df4be73c996
Hudson Sena <107481318+HudsonSena@users.noreply.github.com>
2025-09-17 14:00:44 -0300

    update lab1709

--- algoritmoEstruturaDados01/lab1709.c
+++ algoritmoEstruturaDados01/lab1709.c
@@ -1,21 +1,33 @@
 #include <stdio.h>
-int main(){
+
+int main() {
     int n;
     printf("tamanho matriz:\n");
     scanf("%d", &n);
-    int m[n][n], cont=1;
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            if(m[i-1][j-1] == i || m[i-1][j-1] == 0){
-                m[i][j] = m[i-1][j-1] + 1;
-            }
+    int m[n][n];
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            int cima = i;
+            int esquerda = j;
+            int baixo = n - 1 - i;
+            int direita = n - 1 - j;
+
+            int min = cima;
+            if (esquerda < min) min = esquerda;
+            if (baixo < min) min = baixo;
+            if (direita < min) min = direita;
+
+            m[i][j] = min + 1;
         }
     }
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
             printf("%d ", m[i][j]);
         }
         printf("\n");
     }
+
     return 0;
 }
