e4065c4a200a9201c58eac7051ce141723561e0a
Hudson Sena <107481318+HudsonSena@users.noreply.github.com>
2025-12-17 10:45:00 -0300

    update tarefa004

--- algoritmoEstruturaDados01/tarefa004.c
+++ algoritmoEstruturaDados01/tarefa004.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 
+void aplicar_powerup_pontuacao_dupla(int *pi);
+
 int main() {
     int pontuacao_jogador = 1500;
-    int *ponteiro_p = &pontuacao_jogador;
-    printf("Pontuacao antes do power-up: %d\n", pontuacao_jogador);
-    int power = aplicar_powerup_pontuacao_dupla(ponteiro_p);
-    printf("Pontuacao apos o power-up: %d\n", power);
-
+    printf("Pontuacao antes de power-up: %d\n", pontuacao_jogador);
+    int *pontuacao_power;
+    pontuacao_power = &pontuacao_jogador;
+    aplicar_powerup_pontuacao_dupla(pontuacao_power);
+    printf("Pontuacao apos o power-up: %d\n", pontuacao_jogador);
     return 0;
 }
 
-void aplicar_powerup_pontuacao_dupla(int *pp) {
-    int power = *pp * 2;
-    return power;
+void aplicar_powerup_pontuacao_dupla(int *pi) {
+    *pi = *pi * 2;    
 }
