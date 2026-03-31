77a41a9bac8b0671e3ee7d41def124fdd7b5066e
Hudson Sena <107481318+HudsonSena@users.noreply.github.com>
2026-03-31 11:22:23 -0300

    update class arvores

---
+++ algoritmoEstruturaDados02/arvore001.c
@@ -0,0 +1,50 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct no {
+    int valor;
+    int nfilhos;
+    struct no *filhos[10];
+} No;
+
+No *criarNo(int valor);
+void addFilho(No *pai, No *filho);
+void liberarArvore(No*);
+
+int main() {
+    No *raiz = criarNo(1);
+    No *f[5] = {
+        criarNo(3),
+        criarNo(2),
+        criarNo(5),
+        criarNo(4),
+        criarNo(5)
+    };
+    addFilho(raiz, f[0]);
+    addFilho(raiz, f[1]);
+    addFilho(raiz, f[2]);
+    addFilho(f[0], f[3]);
+    addFilho(f[0], f[4]);
+    liberarArvore(raiz);
+    return 0;
+}
+
+No *criarNo(int valor) {
+    No *r = (No *)malloc(sizeof(No));
+    r->valor = valor;
+    r->nfilhos = 0;
+    return r;
+}
+
+void addFilho(No *pai, No *filho) {
+    pai->filhos[pai->nfilhos++] = filho;
+}
+
+
+void liberarArvore(No* raiz) {
+    for(int i = 0; i < raiz->nfilhos; i++) {
+        liberarArvore(raiz->filhos[i]);
+    }
+    printf("Liberando no: %d\n", raiz->valor);
+    free(raiz);
+}
