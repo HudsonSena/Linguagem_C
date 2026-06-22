8a20fe514ffe106f2ee0a4e17d3ce3b8540242e3
HudsonSena <hudsonsena93@gmail.com>
2026-06-22 12:01:48 -0300

    Fix: Ajuste nos códigos

--- algoritmoEstruturaDados02/bloom_filter/estrutura.c
+++ algoritmoEstruturaDados02/bloom_filter/estrutura.c
@@ -1,31 +1,47 @@
 #define MAX 1000000
-#define NUM_BYTES 32
+#define NUM_BYTES 16
 #include <stdio.h>
 #include <stdlib.h>
 
 int V[MAX] = {0};
+void primos();
+void eliminarMultiplos(int n);
+void convertToBits(int x);
 
 struct filtro_blooom {
-  char bytes[NUM_BYTES];
+  unsigned short bytes[NUM_BYTES];
 };
 
 struct filtro_blooom filtro;
 
-void primos();
 void consultarPrimo(int);
-void eliminarMultiplos(int n);
 int procurarByte(int x);
 int procurarBit(int x);
+void mostrarFiltro();
 int hash1(int);
 int hash2(int);
 int hash3(int);
 void inserir(int, int, int (*f[])(int));
 
-int main(int argc, char *argv[]) { primos(); }
+int main(int argc, char *argv[]) {
+  int x = 28;
+  int (*f[3])(int) = {hash1, hash2, hash3};
+  inserir(x, 3, f);
+  mostrarFiltro();
 
-int procurarByte(int x) { return x / 8; }
+
+}
+
+int procurarByte(int x) { return (x / 8); }
 int procurarBit(int x) { return x % 8; }
 
+void mostrarFiltro() {
+  for (size_t i = 0; i < NUM_BYTES; i++) {
+    printf("%2ld --> %3d --> ", i, filtro.bytes[i]);
+    convertToBits(filtro.bytes[i]);
+    printf("\n");
+  }
+}
 void inserir(int x, int hs, int (*f[])(int)) {
   int h[hs];
   for (int i = 0; i < hs; i++) {
@@ -40,15 +56,31 @@ int hash1(int x) { return x % (NUM_BYTES * 8); }
 int hash2(int x) { return (3 * x + 7) % (NUM_BYTES * 8); }
 int hash3(int x) { return (5 * x + 11) % (NUM_BYTES * 8); }
 
+void convertToBits(int x){
+  int aux =   x;
+  for(size_t i=0; i<8; i++){
+    printf("%d", aux & 1);
+    aux = aux >> 1;
+  }
+}
 void consultarPrimo(int x) {
   int (*f[3])(int) = {hash1, hash2, hash3};
   int h[3] = {0};
+  int c = 0;
   for (int i = 0; i < 3; i++) {
     h[i] = f[i](x);
     int byte = procurarByte(h[i]);
     int bit = procurarBit(h[i]);
-    char b = filtro.bytes[byte];
-    //Construir a máscara para validar se o número é pimo.
+    char B = filtro.bytes[byte];
+    char novo = B & (1 << bit);
+    if (novo == (bit << 1)) {
+      c++;
+    }
+  }
+  if (c == 3) {
+    printf("Quase certeza que é primo\n");
+  } else {
+    printf("De fato não é primo\n");
   }
 }
 
--- algoritmoEstruturaDados02/hash/mainEstrutura.c
+++ algoritmoEstruturaDados02/hash/mainEstrutura.c
@@ -2,6 +2,13 @@
 #include "estrutura01.c"
 #include "estrutura02.c"
 
+/*Aqui mostramos o tamanho em bits de cada estrutura, e vemos que a Estrutura01 tem 16 bytes, 
+e a Estrutura02 tem 24 bytes, isso ocorre por causa do alinhamento de dados,
+onde o compilador alinha os dados para otimizar o acesso à memória,
+e nesse caso a Estrutura01 tem um char antes do long int, o que faz com que o compilador alinhe o long int para 8 bytes,
+e a Estrutura02 tem o long int antes do char, o que faz com que o compilador alinhe o long int para 8 bytes,
+e os chars para 1 byte, totalizando 24 bytes.*/
+
 int main() {
     int x1 = sizeof(Estrutura01);
     int x2 = sizeof(Estrutura02);
